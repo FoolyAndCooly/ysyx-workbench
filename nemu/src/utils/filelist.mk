44ec880d40e2885adf0159d90d2a9e0286fb735c
liuyicheng <1764236936@qq.com>
2025-01-19 15:54:40 +0800

    改成llvm14

--- nemu/src/utils/filelist.mk
+++ nemu/src/utils/filelist.mk
@@ -15,6 +15,6 @@
 
 ifneq ($(CONFIG_ITRACE)$(CONFIG_IQUEUE),)
 CXXSRC = src/utils/disasm.cc
-CXXFLAGS += $(shell llvm-config-13 --cxxflags) -fPIE
-LIBS += $(shell llvm-config-13 --libs)
+CXXFLAGS += $(shell llvm-config-14 --cxxflags) -fPIE
+LIBS += $(shell llvm-config-14 --libs)
 endif
